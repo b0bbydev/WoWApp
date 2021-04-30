26593b8421e73c5ae70e6271a3349e3fcd93b557
Bobby <200338513@student.georgianc.on.ca>
2021-04-29 20:17:49 -0400

    init commit

---
+++ src/main/java/LoginViewController.java
@@ -0,0 +1,181 @@
+
+import com.microsoft.alm.oauth2.useragent.AuthorizationException;
+import com.microsoft.alm.oauth2.useragent.AuthorizationResponse;
+import com.microsoft.alm.oauth2.useragent.UserAgent;
+import com.microsoft.alm.oauth2.useragent.UserAgentImpl;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.ResourceBundle;
+
+public class LoginViewController implements Initializable
+{
+    // instance variables.
+    @FXML
+    private Button loginButton;
+
+    // These are pulled from gradle.properties
+    String blizzardDomain;
+    String clientId;
+    String clientSecret;
+    String redirectUri;
+    String scope;
+    String grantType;
+
+    /**
+     * Loads our config info from the app.properties file
+     *
+     * @throws IOException
+     */
+    public void loadProperties() throws IOException
+    {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("app.properties");
+        Properties appProps = new Properties();
+        appProps.load(inputStream);
+        blizzardDomain = appProps.getProperty("blizzardDomain");
+        clientId = appProps.getProperty("blizzardClientId");
+        clientSecret = appProps.getProperty("blizzardClientSecret");
+        redirectUri = appProps.getProperty("redirectUri");
+        scope = appProps.getProperty("scope");
+        grantType = appProps.getProperty("grantType");
+    }
+
+
+    /**
+     * Build the authorization request URL
+     *
+     * @return
+     * @throws URISyntaxException
+     * @throws MalformedURLException
+     */
+    public URI getAuthorizationEndpointUri() throws URISyntaxException, MalformedURLException
+    {
+        URIBuilder builder = new URIBuilder();
+
+        builder.setScheme("https");
+        builder.setHost(blizzardDomain);
+        builder.setPath("/oauth/authorize");
+        builder.addParameter("client_id", clientId);
+        builder.addParameter("redirect_uri", redirectUri);
+        builder.addParameter("response_type", "code");
+        //builder.addParameter("state", "this is a state");
+        builder.addParameter("scope", scope);
+
+        URL url = builder.build().toURL();
+
+        return url.toURI();
+    }
+
+
+    /**
+     * Requests an authorization code from the auth server
+     *
+     * @return
+     * @throws MalformedURLException
+     * @throws URISyntaxException
+     * @throws AuthorizationException
+     */
+    public String requestAuthCode() throws IOException, URISyntaxException, AuthorizationException
+    {
+        loadProperties();
+        // Generate the auth endpoint URI to request the auth code
+        URI authorizationEndpoint = getAuthorizationEndpointUri();
+
+        System.out.print("Authorization Endpoint URI: ");
+        System.out.println(authorizationEndpoint.toString());
+
+        final URI redirectUri = new URI(this.redirectUri);
+
+        // Create the user agent and make the call to the auth endpoint
+        final UserAgent userAgent = new UserAgentImpl();
+
+        final AuthorizationResponse authorizationResponse = userAgent.requestAuthorizationCode(authorizationEndpoint, redirectUri);
+
+        // We should have the code, which we can trade for the token
+        final String code = authorizationResponse.getCode();
+
+        System.out.print("Authorization Code: ");
+        System.out.println(code);
+
+        return code;
+    }
+
+
+    /**
+     * Given an authorization code, calls the auth server to request a token
+     *
+     * @param code
+     * @return
+     * @throws URISyntaxException
+     * @throws IOException
+     */
+    public String getTokenForCode(String code) throws URISyntaxException, IOException
+    {
+        // The token request URL
+        final String tokenUrl = "https://" + blizzardDomain + "/oauth2/default/v1/token";
+
+        // The original redirect URL
+        final URI redirectUri = new URI(this.redirectUri);
+
+        // Using HttpClient to make the POST to exchange the auth code for the token
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpPost post = new HttpPost(tokenUrl);
+
+        // Adding the POST params to the request
+        List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
+        urlParameters.add(new BasicNameValuePair("grant_type", grantType));
+        urlParameters.add(new BasicNameValuePair("code", code));
+        urlParameters.add(new BasicNameValuePair("redirect_uri", redirectUri.toString()));
+        urlParameters.add(new BasicNameValuePair("client_id", clientId));
+        urlParameters.add(new BasicNameValuePair("client_secret", clientSecret));
+        urlParameters.add(new BasicNameValuePair("scope", scope));
+
+        post.setEntity(new UrlEncodedFormEntity(urlParameters));
+
+        // Execute the request
+        HttpResponse response = client.execute(post);
+
+        // Print the status code
+        System.out.println("Response Code : " + response.getStatusLine().getStatusCode());
+
+        // Get the content as a String
+        String content = EntityUtils.toString(response.getEntity());
+
+        System.out.println("Result : " + content.toString());
+
+        return content.toString();
+    }
+
+
+    @FXML
+    void login() throws AuthorizationException, IOException, URISyntaxException
+    {
+        requestAuthCode();
+    }// end of login().
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+
+    }// end of initialize().
+}// end of class.
---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class Main extends Application
+{
+    public static void main(String[] args)
+    {
+        launch(args);
+    }// end of main.
+
+    @Override
+    public void start(Stage stage) throws IOException
+    {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("LoginView.fxml")));
+        Scene scene = new Scene(root);
+        //root.getStylesheets().add(Objects.requireNonNull(getClass().getResource("Assets/main.css")).toExternalForm());
+        stage.setScene(scene);
+        stage.setTitle("World of Warcraft Application");
+        stage.show();
+    }// end of start().
+}// end of class.
